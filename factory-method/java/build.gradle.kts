ad56817a5f62d417588532a8016076691dbcbcc6
NoOneHardy <silas.hardegger@outlook.com>
2026-01-19 14:12:38 +0100

    feature: gradle project (#1)

---
+++ factory-method/java/build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    java
+}
+
+group = "ch.no1hardy"
+version = "1.0.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
