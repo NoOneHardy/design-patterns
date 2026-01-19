17464653567ef118344dfa543b701c99ae0d3d63
NoOneHardy <silas.hardegger@outlook.com>
2026-01-19 14:19:18 +0100

    feature: refactor gradle configuration (#1)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,23 @@
+plugins {
+    java
+}
+
 version = "1.0.0"
 
-repositories {
-    mavenCentral()
+subprojects {
+    plugins.apply("java")
+
+    repositories {
+        mavenCentral()
+    }
+
+    dependencies {
+        testImplementation(platform("org.junit:junit-bom:5.10.0"))
+        testImplementation("org.junit.jupiter:junit-jupiter")
+        testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    }
+
+    tasks.test {
+        useJUnitPlatform()
+    }
 }
