638a1e028d591f851568441b1a7498f9824fd88c
NoOneHardy <silas.hardegger@outlook.com>
2026-01-26 15:37:35 +0100

    refactor: setup decorator java (#9)

---
+++ decorator/java/build.gradle.kts
@@ -0,0 +1,6 @@
+plugins {
+    java
+}
+
+group = "ch.no1hardy"
+version = "1.0.0"
---
+++ decorator/java/src/main/java/ch.no1hardy/App.java
@@ -0,0 +1,7 @@
+package ch.no1hardy;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello from Java Adapter!");
+    }
+}
